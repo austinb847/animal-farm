2bb1552560f3a0c48ed0f73bb7075fd7705511b2
austinb847 <austinb847@icloud.com>
2020-04-20 13:53:08 -0700

    meaningful fail test for food level after 3 seconds

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,17 +12,21 @@ afterEach(function() {
   jest.clearAllTimers();
 });
 
-test('should have a name, food level, health level, energy level, sleep level, annoyed level, and snuggle level when created', () => {
+test('should have a name, food level, health level, energy level, sleep level, annoyed level, bathroom level, and snuggle level when created', () => {
   expect(coco.name).toEqual("Coco");
   expect(coco.foodLevel).toEqual(10);
   expect(coco.healthLevel).toEqual(100);
   expect(coco.energyLevel).toEqual(10);
   expect(coco.sleepLevel).toEqual(10);
   expect(coco.snuggleLevel).toEqual(5);
-  expect(coco.annoyedLevel).toEqual(0);
+  expect(coco.annoyedLevel).toEqual(0)
+  expect(coco.bathroomLevel).toEqual(false);
 });
 
-
+test('should have a food level of 5 after 3001 milliseconds', () => {
+  jest.advanceTimersByTime(3001);
+  expect(coco.foodLevel).toEqual(5);
+});
 
 
 
