c6712d914a62799aa5c7d4939797d603560fe66a
austinb847 <austinb847@icloud.com>
2020-04-20 11:24:39 -0700

    create pet class and set name and all levels for pet

---
+++ src/pet.js
@@ -0,0 +1,11 @@
+export class Pet {
+  constructor(name) {
+    this.name = name;
+    this.foodLevel = 10;
+    this.healthLevel = 100;
+    this.energyLevel = 10;
+    this.sleepLevel = 10;
+    this.snuggleLevel = 5;
+    this.annoyedLevel = 0;
+  }
+}
