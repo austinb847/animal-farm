2da44f643dede1e67f92996b183e25559387158f
austinb847 <austinb847@icloud.com>
2020-04-21 15:25:22 -0700

    add env plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 
 module.exports = {
   entry: './src/main.js',
@@ -15,6 +17,7 @@ module.exports = {
   },   
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
+    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Animal Farm',
