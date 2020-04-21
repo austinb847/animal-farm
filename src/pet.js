e0c74644d1ad0ec3e09dbe0d257ffb5a6ee7f298
austinb847 <austinb847@icloud.com>
2020-04-21 10:12:43 -0700

    create walk method, didYouGetFed method, stopWalking method and gotToSleep method

--- src/pet.js
+++ src/pet.js
@@ -4,19 +4,52 @@ export class Pet {
     this.foodLevel = 10;
     this.healthLevel = 100;
     this.energyLevel = 10;
-    this.sleepLevel = 10;
+    this.sleepLevel = 0;
     this.snuggleLevel = 5;
     this.annoyedLevel = 0;
-    this.bathroomLevel = false;
+    this.bathroomLevel = 0;
+    this.alive = true;
+    this.asleep = false;
   }
 
   setHunger() {
     setInterval( () => {
-      this.foodLevel -= 5;
+      this.foodLevel -= 2;
     }, 20000);
   }
 
   feed() {
     this.foodLevel = 10;
   }
+
+  didYouGetFed() {
+    setInterval(() => {
+      if(this.foodLevel > 0) {
+        return true;
+      } else {
+        this.healthLevel -= 10;
+      }
+    }, 100000);
+  }
+
+  walk() {
+    setInterval( () => {
+      this.foodLevel -= 1;
+      this.sleepLevel += 1;
+      this.energyLevel -= 1;
+    }, 1000);
+  }
+
+  stopWalking() {
+    clearInterval(this.walk);
+  }
+
+  goToSleep() {
+    this.asleep = true;
+    this.healthLevel += 10;
+    setTimeout(() => {
+      this.asleep = false
+    }, 20000);
+  }
+
 }
