d61f4096148cab82c3b4318b521cb4734b941c86
austinb847 <austinb847@icloud.com>
2020-04-21 10:31:27 -0700

    add test for goToSleep method, succesfully passes

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,6 +7,7 @@ describe('Pet', () => {
   beforeEach(function() {
     coco = new Pet("Coco");
     coco.setHunger();
+    coco.didYouGetFed();
   });
 
   afterEach(function() {
@@ -18,23 +19,42 @@ describe('Pet', () => {
     expect(coco.foodLevel).toEqual(10);
     expect(coco.healthLevel).toEqual(100);
     expect(coco.energyLevel).toEqual(10);
-    expect(coco.sleepLevel).toEqual(10);
+    expect(coco.sleepLevel).toEqual(0);
     expect(coco.snuggleLevel).toEqual(5);
     expect(coco.annoyedLevel).toEqual(0)
-    expect(coco.bathroomLevel).toEqual(false);
+    expect(coco.bathroomLevel).toEqual(0);
+    expect(coco.alive).toEqual(true);
+    expect(coco.asleep).toEqual(false);
   });
 
-  test('should have a food level of 5 after 20 seconds', () => {
+  test('should have a food level of 8 after 20 seconds', () => {
     jest.advanceTimersByTime(20001);
-    expect(coco.foodLevel).toEqual(5);
+    expect(coco.foodLevel).toEqual(8);
   });
 
   test('should have a food level of 10 if pet is fed', function() {
+    jest.advanceTimersByTime(19001)
     coco.feed();
     expect(coco.foodLevel).toEqual(10);
   });
 
-  
+  test('should lose 10 health if 100 seconds pass without being fed', function() {
+    jest.advanceTimersByTime(100001);
+    expect(coco.healthLevel).toEqual(90);
+  });
+
+  test('should have asleep set to true, add 10 to health, and then change asleep to false and put sleep lvl back to 0 after 20 seconds', function() {
+    coco.healthLevel = 90;
+    coco.goToSleep();
+    expect(coco.asleep).toEqual(true);
+    expect(coco.healthLevel).toEqual(100);
+    jest.advanceTimersByTime(20001);
+    expect(coco.asleep).toEqual(false);
+    expect(coco.sleepLevel).toEqual(0);
+    
+  });
+
+
 
 
 });
